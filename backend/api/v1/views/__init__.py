5439cdea675acb7db1067f1ac11be10f1597b9c2
thenumber1boss <bamijokoseun@gmail.com>
2023-12-19 01:55:42 +0100

    deleted some files

--- backend/api/v1/views/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3.9
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from backend.api.v1.views.product import *
-from backend.api.v1.views.vendor import *
